b611aec7a82ba0bb27d983dd1e7bee23744e5b52
A1doranM <vaselisk1997@gmail.com>
2023-06-19 11:47:11 +0300

    Added project starter code

---
+++ server/index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const fs = require("node:fs");
+const http2 = require("node:http2");
+
+
+const routing = {
+  "/": "<h1>welcome to homepage</h1><hr>",
+  "/user": "user",
+  "/api/method1": (req, res) => {
+    console.log(req.url + " " + res.statusCode);
+    return { status: res.statusCode };
+  },
+};
+
+const types = {
+  object: JSON.stringify,
+  string: (s) => s,
+  undefined: () => "not found",
+  function: (fn, req, res) => JSON.stringify(fn(req, res)),
+};
+
+const key = fs.readFileSync("./utils/cert/privateKey.pem");
+const cert = fs.readFileSync("./utils/cert/certificate.crt");
+const options = { key, cert };
+
+const server = http2.createSecureServer(options, (req, res) => {
+  const data = routing[req.url];
+  const type = typeof data;
+  const serializer = types[type];
+  const result = serializer(data, req, res);
+  res.end(result);
+});
+
+server.listen(8000);
+console.log("Open: https://127.0.0.1:8000");
+
---
+++ server/utils/cert/generate.sh
@@ -0,0 +1,8 @@
+#cd "$(dirname "$0")"
+#openssl genrsa -out key.pem 3072
+#openssl req -new -out self.pem -key key.pem -subj "/CN=localhost"
+#openssl req -text -noout -in self.pem
+#openssl x509 -req -days 1024 -in self.pem -signkey key.pem -out cert.pem -extfile generate.ext
+
+openssl req -x509 -newkey rsa:2048 -nodes -sha256 -subj '/CN=localhost' \
+  -keyout localhost-privkey.pem -out localhost-cert.pem
